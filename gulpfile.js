c5ff1729c90e37d245f63485e0cf9ed3ec7851f0
Alan Sánchez Pérez Peña <alan247@gmail.com>
2016-01-13 18:33:06 -0600

    Fix gulp and bower settings

--- gulpfile.js
+++ gulpfile.js
@@ -4,9 +4,44 @@ var browserSync = require('browser-sync').create();
 var reload = browserSync.reload;
 var minifyCSS = require('gulp-minify-css');
 var concat = require('gulp-concat');
+var minifyHTML = require('gulp-htmlmin');
+var eslint = require('gulp-eslint');
+var mainBowerFiles = require('main-bower-files');
+var filter = require('gulp-filter');
+var sass = require('gulp-sass');
+var order = require('gulp-order');
 
-gulp.task('copy', function(){
+gulp.task('bower-scripts', function(){
+	gulp.src(mainBowerFiles())
+		.pipe(filter('*.js'))
+		.pipe(order([
+			'modernizr.js',
+			'jquery.js',
+			'foundation.js',
+			'knockout.js'
+		]))
+		.pipe(concat('vendors.js'))
+		.pipe(uglify())
+		.pipe(gulp.dest('./dist/js'))
+});
+
+gulp.task('bower-styles', function(){
+	gulp.src(mainBowerFiles())
+		.pipe(filter(['*.scss', '*.css']))
+		.pipe(order([
+			'normalize.css',
+			'*'
+		]))
+		.pipe(sass().on('error', sass.logError))
+		.pipe(concat('vendors.css'))
+		.pipe(minifyCSS())
+		.pipe(gulp.dest('./dist/css/'));
+});
+
+
+gulp.task('html', function(){
 	gulp.src('./src/index.html')
+		.pipe(minifyHTML({collapseWhitespace: true}))
 		.pipe(gulp.dest('./dist/'))
 		.pipe(reload({stream: true}))
 });
@@ -15,6 +50,8 @@ gulp.task('js', function(){
 	gulp.src('./src/js/*.js')
 		.pipe(uglify())
 		.pipe(concat('app.js'))
+		.pipe(eslint())
+		.pipe(eslint.format())
 		.pipe(gulp.dest('./dist/js'))
 		.pipe(reload({stream: true}))
 });
@@ -26,15 +63,15 @@ gulp.task('css', function(){
 		.pipe(reload({stream: true}))
 });
 
-gulp.task('serve', function() {
+gulp.task('serve', function(){
 	browserSync.init({
 		server: {
 			baseDir: "./dist/"
 		}
 	});
-	gulp.watch('./src/index.html', ['copy']);
+	gulp.watch('./src/index.html', ['html']);
 	gulp.watch('./src/js/*.js', ['js']);
 	gulp.watch('./src/css/*.css', ['css']);
 });
 
-gulp.task('default', ['copy', 'js', 'css']);
+gulp.task('default', ['bower-styles', 'bower-scripts', 'html', 'js', 'css', 'serve']);
