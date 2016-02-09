14a3d841aa5bb52a2b61bfce4e6c3c158795a03e
Alan Sánchez Pérez Peña <alan247@gmail.com>
2016-02-09 15:54:45 +0100

    Update

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,4 @@
 
-
-
 	$('input').on('focus keydown', function(){
 		if ($(window).width() <= 1024){
 			$('.toggle-controls').css('display', 'none');
@@ -14,4 +12,36 @@
 			$('.toggle-controls, .overlay, .list').toggle();
 		}
     });
+
+
+
+
+
+var ViewModel = function() {
+	var self = this;
+
+	self.initializeMap = function() {
+		var mapCanvas = document.getElementById('map');
+        var mapOptions = {
+	      	center: new google.maps.LatLng(49.8333467,14.1009912),
+	      	zoom: 3,
+	      	mapTypeId: google.maps.MapTypeId.ROADMAP,
+	      	disableDefaultUI: true,
+	      	zoomControl: true
+        }
+        var map = new google.maps.Map(mapCanvas, mapOptions);
+        var marker = new google.maps.Marker({
+        	position: new google.maps.LatLng(19.3911658,-99.4245054),
+        	title: 'Hola',
+        	map: map
+        });
+	}
+	self.initializeMap();
+}
+
+ko.applyBindings(new ViewModel());
+
+
+
+
 
